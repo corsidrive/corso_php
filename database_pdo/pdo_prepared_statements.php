1806f66d206c59c932d81b55506519e0f6049e75
roberto <sarassoroberto@gmail.com>
2018-07-06 07:23:43 +0200

    prepare statement

--- database_pdo/pdo_prepared_statements.php
+++ database_pdo/pdo_prepared_statements.php
@@ -13,13 +13,26 @@ try {
     $connessione = new PDO("mysql:host=$host;dbname=$db", $user, $password);
     $connessione->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "Connessione a MySQL tramite PDO effettuata.";
+    echo "Connessione a MySQL tramite PDO effettuata.\n";
     
     $nome = 'Kendra';
 
     //SELECT * FROM `user` WHERE first_name LIKE 'K%'
-    $query = $connessione->prepare("SELECT * FROM `user` WHERE first_name = :ricerca;"); 
+    //$query = $connessione->prepare("SELECT * FROM `user` WHERE first_name = :ricerca;"); 
+    $query = $connessione->prepare("SELECT * FROM `user`"); 
+
+    print_r($query);
     $query->bindParam(':ricerca',$nome,PDO::PARAM_STR);
+    $query->execute();
+
+    echo "Trovati:".$query->rowCount();
+
+    $res = $query->fetch();
+    print_r($res);
+
+    $res = $query->fetch();
+    print_r($res);
+
 }
 catch(PDOException $e)
 {
